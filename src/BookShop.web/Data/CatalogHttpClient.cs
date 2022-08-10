d143a37e6c449c5898aacbabcc42e4562e9774c7
Guillaume <guillaume@techene.net>
2022-08-10 15:57:39 +0200

    First draft of front implementing paging.

--- src/BookShop.web/Data/CatalogHttpClient.cs
+++ src/BookShop.web/Data/CatalogHttpClient.cs
@@ -11,11 +11,11 @@ public class CatalogHttpClient
         _httpClient = httpClient;
     }
 
-    public Task<Catalog?> GetCatalog() =>
-        _httpClient.GetFromJsonAsync<Catalog>("/api/Catalog?Currency=EUR");
+    public Task<Catalog?> GetCatalog(int requestedPage = 1) =>
+        _httpClient.GetFromJsonAsync<Catalog>($"/api/Catalog?Currency=EUR&PageNumber={requestedPage}");
 
 }
 
-public record Catalog(Book[] Books);
+public record Catalog(Book[] Books, int TotalNumberOfPages);
 
 public record Book(string ISBN, string Title, string Author, string PictureUrl, int Quantity, Price UnitPrice);
